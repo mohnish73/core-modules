b7a110789e19a6284f15203dce8c454f0bb94476
mohnish.kumar <mohnish.kumar@binarysemantics.com>
2026-04-23 23:42:22 +0530

    fix: add materialIconsExtended to cmp-ui-components, bump to 1.0.7

--- cmp/ui-components/build.gradle.kts
+++ cmp/ui-components/build.gradle.kts
@@ -23,6 +23,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.animation)
             implementation(compose.components.uiToolingPreview)
